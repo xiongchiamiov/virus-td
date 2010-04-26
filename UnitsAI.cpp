aa35943701e4155c25e70c5cac6a67fba065a249
jtrobins <jtrobins@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-04-26 03:08:37 +0000

    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@8 05766cc9-4f33-4ba7-801d-bd015708efd9

--- UnitsAI.cpp
+++ UnitsAI.cpp
@@ -10,7 +10,17 @@ UnitsAI::~UnitsAI(void)
 
 void UnitsAI::determineUnitsPaths() {
 	std::list<Unit*>::iterator i;
-	bool grid[16][32];
+
+	// will be taken out once i establish how to get the current grid
+	// begin
+	int G_WIDTH = 16;	
+	int G_HEIGHT = 32;
+	int goalx = 15;
+	int goaly = 31;
+	int startx = 0;
+	int starty = 0;
+	bool grid[G_WIDTH][G_HEIGHT];
+	// end
 
 	for(i = uList.begin(); i != uList.end(); ++i) {
 		
