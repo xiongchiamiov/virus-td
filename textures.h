8a7bea274f3325049105024ed984b32cee9b3f16
kehung <kehung@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-04-29 03:31:45 +0000

    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@25 05766cc9-4f33-4ba7-801d-bd015708efd9

--- textures.h
+++ textures.h
@@ -1,5 +1,15 @@
 #include <stdlib.h>
-#include <gl/glut.h>
+ #ifdef __unix__
+     #include <GL/glut.h>
+ #endif
+
+ #ifdef __APPLE__
+     #include "GLUT/glut.h"
+ #endif
+
+ #ifdef _WIN32_ 
+     #include <gl/glut.h>
+ #endif
 #include <stdio.h>
 #pragma once
 #include <iostream>
