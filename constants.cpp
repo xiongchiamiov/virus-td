abedff2dbc1fef5c676c8c95854f979aeeee3558
agonza40 <agonza40@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-05-01 19:46:26 +0000

    Added some tower and unit costs/income bonuses
    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@42 05766cc9-4f33-4ba7-801d-bd015708efd9

--- constants.cpp
+++ constants.cpp
@@ -4,6 +4,25 @@ const int GRID_HEIGHT = 32;
 const float GRID_SIZE = 0.25;
 const int CYCLE_TIME = 25000;
 
+namespace unit_cost{
+  const int BASIC = 5;
+  const int FAST = 10;
+  const int STRONG = 20;
+}
+namespace unit_bonus{
+  extern const int BASIC = 1;
+  extern const int FAST = 2;
+  extern const int STRONG = 5;
+}
+namespace tower_cost{
+  const int BASIC = 5;
+  const int FAST = 5;
+  const int FREEZE = 10;
+  const int SLOW = 25;
+  const int TRAP = 15;
+  const int WALL = 2;
+}
+
 g_elem loc2grid(float x, float z) {
 	return g_elem((int)((x - GRID_SIZE)/(2.0 * GRID_SIZE) + 0.5), (int)((z - GRID_SIZE)/(2.0 * GRID_SIZE)  + 0.5));
 }
--- constants.h
+++ constants.h
@@ -3,9 +3,25 @@ extern const int GRID_WIDTH;
 extern const int GRID_HEIGHT;
 extern const float GRID_SIZE;
 extern const int CYCLE_TIME;
-//static const int GRID_WIDTH = 16;
-//static const int GRID_HEIGHT = 32;
-//static const float GRID_SIZE = 0.25;
+
+namespace unit_cost{
+  extern const int BASIC;
+  extern const int FAST;
+  extern const int STRONG;
+}
+namespace unit_bonus{
+  extern const int BASIC;
+  extern const int FAST;
+  extern const int STRONG;
+}
+namespace tower_cost{
+  extern const int BASIC;
+  extern const int FAST;
+  extern const int FREEZE;
+  extern const int SLOW;
+  extern const int TRAP;
+  extern const int WALL;
+}
 
 struct g_elem{
   int x;
