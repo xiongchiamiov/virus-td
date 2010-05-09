46b560be5b86346c3ce14293b92e823c8cafbdd1
agonza40 <agonza40@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-05-09 03:19:13 +0000

    Removed unnecessary #include from models.h
    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@67 05766cc9-4f33-4ba7-801d-bd015708efd9

--- models.h
+++ models.h
@@ -1,15 +1,3 @@
-#ifdef __unix__
-   #include <GL/glut.h>
-#endif
-
-#ifdef __APPLE__
-   #include "GLUT/glut.h"
-#endif
-
-#ifdef _WIN32 
-   #include <gl/glut.h>
-#endif
-
 #include "lighting.h"
 
 void drawWorm();
