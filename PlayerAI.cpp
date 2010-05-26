44330505d0d6f0dc8b4f0534d16d60092ee76631
pearson <pearson@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-05-26 20:10:02 +0000

    PlayerAI: place towers according to predetermined layout, rather than at random.
    This also adds an easy way to check "how much" we want to build new towers (rather than send units, or save up resources, or upgrade, or whatever).  For now, though, we're building new towers as long as there are some to be built.
    
    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@122 05766cc9-4f33-4ba7-801d-bd015708efd9

--- PlayerAI.cpp
+++ PlayerAI.cpp
@@ -4,6 +4,7 @@ PlayerAI::PlayerAI(void) {
 	towers = 0;
 	units = 0;
 	updates = 0;
+	targetNumTowers = NULL;
 }
 PlayerAI::~PlayerAI(void) {}
 
@@ -12,29 +13,19 @@ void PlayerAI::update(int dt) {
 	
 	// we don't want to do anything right at the very, very beginning
 	if (updates) {
-		if (!(updates % 500)) {
-			/* randomly place towers
-			 *
-			 * NUMBER_OF_TOWERS is the number of different towers we can build
-			 * STARTING_TOWER is the identifier for the lower-numbered tower
-			 * These need to be modified when the towers in Player are changed,
-			 * which means we probably shouldn't have them defined here. Oh well.
-			 */
-			srand(0);//time(NULL));
-			const int NUMBER_OF_TOWERS = 6;
-			const int STARTING_TOWER = 12;
-			
-			int i = 0;
-			while(player.placeTower(rand() % player.pGrid.G_WIDTH + 1,
-			                        rand() % player.pGrid.G_HEIGHT + 1,
-			                        rand() % NUMBER_OF_TOWERS + STARTING_TOWER)
-			   == INVALID_LOCATION
-			   && i < 50) {
-				// increment this so we don't go into infinite loops
-				// when there are no available spots to place towers
-				i++;
-			}
+		if (targetNumTowers == NULL) {
+			build_preferred_tower_layout();
+			targetNumTowers = towersToBuild.size();
 		}
+		
+		if (need_more_towers() > 0) {
+			pair<int, int> coordinates = towersToBuild.top();
+			towersToBuild.pop();
+			player.placeTower(coordinates.first, coordinates.second, 12);
+			cout << towersToBuild.size() << "/" << targetNumTowers << "towers left to build"
+			     << " (" << need_more_towers() << "%)." << endl;
+		}
+		
 		if (!(updates % 200)) {
 			player.spawnUnit(7);
 		}
@@ -42,4 +33,34 @@ void PlayerAI::update(int dt) {
 	
 	player.update(dt);
 	updates++;
+}
+
+void PlayerAI::build_preferred_tower_layout() {
+	// for now, we'll totally just fake this
+	towersToBuild.push(make_pair(6, 30));
+	towersToBuild.push(make_pair(9, 30));
+	towersToBuild.push(make_pair(4, 30));
+	towersToBuild.push(make_pair(2, 30));
+	towersToBuild.push(make_pair(0, 30));
+	towersToBuild.push(make_pair(1, 27));
+	towersToBuild.push(make_pair(3, 27));
+	towersToBuild.push(make_pair(5, 27));
+	towersToBuild.push(make_pair(7, 27));
+	towersToBuild.push(make_pair(13, 28));
+	towersToBuild.push(make_pair(14, 26));
+	towersToBuild.push(make_pair(15, 23));
+	towersToBuild.push(make_pair(13, 23));
+	towersToBuild.push(make_pair(11, 23));
+	towersToBuild.push(make_pair(9, 23));
+	towersToBuild.push(make_pair(7, 23));
+	towersToBuild.push(make_pair(5, 23));
+}
+
+// returns from 0 to 100, inclusive
+int PlayerAI::need_more_towers() {
+	// just the percentage of towers built
+	if (towersToBuild.size() == 0) {
+		return 0; // avoid divide-by-zero errors
+	}
+	return (int)((((float)towersToBuild.size()) / targetNumTowers) * 100);
 }
--- PlayerAI.h
+++ PlayerAI.h
@@ -7,9 +7,13 @@ class PlayerAI {
     PlayerAI(void);
     ~PlayerAI(void);
     void update(int dt);
+	void build_preferred_tower_layout();
+	int need_more_towers();
     Player player;
   private:
     int towers;
     int units;
     int updates;
+	int targetNumTowers; // the number of towers we'd like to have
+	stack< pair<int, int> > towersToBuild; // just (x,y) coordinates
 };
