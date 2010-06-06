dcc92d39b48079f4dfd21285df008312268e6a8c
agonza40 <agonza40@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-06-06 03:08:09 +0000

    Fixed #pragma
    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@182 05766cc9-4f33-4ba7-801d-bd015708efd9

--- GameSounds.h
+++ GameSounds.h
@@ -1,10 +1,10 @@
 #pragma once
-#define VTD_SOUND
+//#define VTD_SOUND
+#ifdef VTD_SOUND
 #include "include/irrKlang.h"
-#include "MyVector.h"
-#pragma comment(lib, "C:\\Users\\Andy\\Documents\\Programs and Source Files\\Libraries\\irrKlang-1.3.0\\lib\\Win32-visualStudio\\irrKlang.lib")
-
 using namespace irrklang;
+#endif
+#include "MyVector.h"
 
 namespace gamesounds{
   void playSound(const char* filename, float x, float y, float z);
@@ -17,7 +17,6 @@ class GameSounds
 public:
   GameSounds(void);
   ~GameSounds(void);
-  void playSound(const char* fileName, vec3df pos);
   void toggleMusic();
 private:
 #ifdef VTD_SOUND
