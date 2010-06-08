eadf7189a4469fde92d07f51c43582eef8bdd116
agonza40 <agonza40@05766cc9-4f33-4ba7-801d-bd015708efd9>
2010-06-08 05:12:02 +0000

    Messing with unit deaths
    git-svn-id: svn+ssh://fileserverness.ath.cx/serv/svn/virus-td@235 05766cc9-4f33-4ba7-801d-bd015708efd9

--- Player.cpp
+++ Player.cpp
@@ -3,7 +3,7 @@
 namespace vtd_player{
   const int START_LIVES = 5;
   const int START_INCOME = 10;
-  const int START_RESOURCES = 1500000; /* originally 10, but 100000 for debug */
+  const int START_RESOURCES = 15;//00000; /* originally 10, but 100000 for debug */
 }
 const int cleanup_dt = 500;
 int last_cleanup = 0;
@@ -143,7 +143,12 @@ void Player::update(int dt){
   std::list<Tower*>::iterator t;
   for(t = tList.begin(); t != tList.end(); ++t){
     (*t)->step(dt);
-    resources += (*t)->getKill();
+    int k =  (*t)->getKill();
+    if(k){
+      resources += k;
+      Particles part(0.7);
+      projectiles.addProjectile(&part, NULL, (*t)->getX() + pos.getX(), (*t)->getY() + pos.getY(), (*t)->getZ() + pos.getZ());
+    }
   }
   for(t = tList.begin(); t != tList.end(); ++t){
     (*t)->retarget();
